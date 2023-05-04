6d0354028fb2e4537f7e53093064f401a4e1c0d3
dfd1123 <dfd1123@naver.com>
2023-05-04 13:58:42 +0900

    :label: add type & interface

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, ReactNode } from 'react';
+import { FunctionComponent, MutableRefObject, ReactNode } from 'react';
 import ModalProvider from './provider/ModalProvider';
 import ModalTemplate from './components/ModalTemplate';
 import useModal from './hooks/useModal';
@@ -12,7 +12,17 @@ export interface ModalType {
   reject: (reason?: any) => void;
 }
 
-export interface ModalComponentPropsType {
+export interface ModalContextType {
+  className?: string;
+  modals: React.MutableRefObject<ModalType[]>;
+  showDim?: boolean;
+  animation?: AnimationOptions;
+  setModals: (modals: ModalType[]) => void;
+  scrollRelease?: () => void;
+  scrollFreeze?: () => void;
+}
+
+export interface ModalPropsType {
   children?: ReactNode;
   className?: string;
   close?: () => void;
@@ -21,14 +31,29 @@ export interface ModalComponentPropsType {
 
 export interface AnimationOptions {
   duration?: number;
-  className?: string;
+  timingFunction?: string;
+  name?: string;
 }
-type PropsOf<T> = T extends FunctionComponent<infer P> ? P : {};
 
-export type AddModalType = <T extends FunctionComponent<any>>(params : { component: T, props?: PropsOf<T>, duplicateCheck?: boolean, isScrollFreeze?: boolean }) => Promise<any>;
-export type OpenModalType = <T extends FunctionComponent<any>>(component: T, props?: PropsOf<T>, duplicateCheck?: boolean) => Promise<any>;
+export interface UseModalHookReturn {
+  modals: MutableRefObject<ModalType[]>;
+  modal: OpenModalType;
+  scrollModal: OpenModalType;
+  closeModal: CloseModalType;
+  resolveModal: ResolveModalType;
+  checkModal: CheckModalType;
+  resetModal: () => void;
+  scrollRelease: (() => void) | undefined;
+}
+
+type PropsOf<T> = (T extends FunctionComponent<infer P> ? P : {}) & { animation?: AnimationOptions; duplicateCheck?: boolean; };
+
+export type AddModalType = <T extends FunctionComponent<any>>(params : { component: T, props?: PropsOf<T>, isScrollFreeze?: boolean }) => Promise<any>;
+export type OpenModalType = <P = any, T extends FunctionComponent<any> = FunctionComponent>(component: T, props?: PropsOf<T>) => Promise<P>;
 export type CloseModalType = (id: number) => void;
 export type ResolveModalType = <T extends ModalType, R>(modal: T, result: R) => void;
 export type CheckModalType = <T extends FunctionComponent>(component: T, onlyLastCheck?: boolean) => boolean;
+
+export const DEFAULT_ANIMATION_DURATION = 250;
 
 export { ModalProvider, ModalTemplate, useModal, useModalAnimation };
